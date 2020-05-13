c223f62b7664fb8f2e7fdaa6791661b2df13ebd0
Aleix Gabarró <aleix.gab@gmail.com>
2020-05-13 03:05:24 +0200

    out Lerp + cross Product
    This calculations are now in shader

--- Motor Demo/ParticleLibrary/PartMath.h
+++ Motor Demo/ParticleLibrary/PartMath.h
@@ -5,26 +5,24 @@ class PartVec2
 {
 public:
 	PartVec2() {}
-
 	PartVec2(float x, float y);
-
+#pragma region XY Components
 public:
 	//X component.
 	float x = 0.0f;
 	//Y component.
 	float y = 0.0f;
+#pragma endregion
 };
 
 class PartVec3
 {
 public:
 	PartVec3() {}
-
 	PartVec3(float x, float y, float z);
 	PartVec3(float x);
 
 	PartVec3 Normalize();
-	PartVec3 CrossProduct(const PartVec3& vec);
 
 	PartVec3& operator =(const PartVec3& vec);
 	PartVec3& operator -();
@@ -33,7 +31,6 @@ public:
 	PartVec3& operator /=(const float num);
 	PartVec3& operator *=(const float num);
 	PartVec3 operator*(const float num);
-	PartVec3 operator*(const PartVec3& vec);
 #pragma region XYZ Components
 public:
 	//X component.
@@ -50,11 +47,8 @@ class PartVec4
 public:
 	PartVec4() {}
 	PartVec4(float x, float y, float z, float w);
-	PartVec4(PartVec2 xy, float z, float w);
 	PartVec4(float x);
 
-	//Lerp between this and a final value
-	PartVec4 PartLerp(PartVec4 final, float t);
 public:
 	//X component.
 	float x = 0.0f;
